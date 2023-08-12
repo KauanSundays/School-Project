aae62f9debf5720a027374c11d22df9eb681181e
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-08-12 10:27:12 -0300

    feat: "WIP"

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
@@ -18,4 +18,13 @@ class Student extends Model
         'phone_number',
     ];
 
+    public function class()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
 }
