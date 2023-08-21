c24ccdaa5aa705dcd930a71c33c351fee90cd3dc
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-08-21 16:05:42 -0300

    feat: "Widgets custom"

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,15 +2,22 @@
 
 namespace App\Filament\Widgets;
 
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use App\Models\Classes;
+use App\Models\Section;
+use App\Models\Student;
 use Filament\Widgets\StatsOverviewWidget\Card;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatsOverview extends BaseWidget
 {
+    protected static ?int $sort = 1;
+
     protected function getCards(): array
     {
         return [
-            //
+            Card::make('Total Students', Student::count()),
+            Card::make('Total Classes', Classes::count()),
+            Card::make('Total Sections', Section::count()),
         ];
     }
 }
